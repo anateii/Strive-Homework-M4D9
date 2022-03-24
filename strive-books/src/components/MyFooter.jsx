595f2399b0abd8e6abbd32371884c51ecd54401b
Ana Matei <anamatei.099@gmail.com>
2022-03-24 13:55:33 +0000

    Added Strive-Books

---
+++ strive-books/src/components/MyFooter.jsx
@@ -0,0 +1,11 @@
+import { Navbar, Button } from "react-bootstrap";
+
+const MyFooter = () => {
+  return (
+    <Navbar bg="dark" variant="dark" className="justify-content-center">
+      <Navbar.Brand href="#home">StriveBooks</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default MyFooter;
---
+++ strive-books/src/components/Welcome.jsx
@@ -0,0 +1,19 @@
+import { Jumbotron, Container } from "react-bootstrap";
+
+
+
+
+
+const Welcome = () => (
+  <Jumbotron fluid>
+    <Container >
+      <h1>Welcome to StriveBooks</h1>
+      <p>
+        "I am not afraid of storms, for I am learning how to sail my ship" -
+        Louisa May Alcott
+      </p>
+    </Container>
+  </Jumbotron>
+);
+
+export default Welcome;
